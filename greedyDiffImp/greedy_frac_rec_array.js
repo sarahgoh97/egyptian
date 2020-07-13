633ac39681a8c577fa995a68a48be1210cbaebdb
sarahgoh97 <sarahgohshining97@yahoo.com.sg>
2020-07-13 10:57:06 +0800

    Greedy variations - recursive function with fraction data objects - added fractions as data objects (instead of represented by 2 separate variables) - recursive calls for greedy and integer division

---
+++ greedyDiffImp/greedy_frac_rec_array.js
@@ -0,0 +1,69 @@
+/* Greedy method with chapter 2 data abstraction
+ * naming of elements with const, except the array
+ * recursive calling functions (greedy, intDivision functions)
+ * using fraction objects
+ * returns an array */
+function greedy(frac) {
+    if (getNum(frac) === 0) {
+        return [];
+    } else {
+        let ans = [];
+        //largest unit fraction = the integer value that is at least den / num
+        const unit_fraction_den = ceiling(getNum(frac), getDen(frac));
+        const next_fraction = simplify(
+            make_frac(getNum(frac) * unit_fraction_den - getDen(frac), getDen(frac) * unit_fraction_den)
+            );
+        ans.push(unit_fraction_den);
+        return ans.concat(greedy(next_fraction));
+    }
+}
+/* similar format to functions found in chapter 1
+ * getting the ceiling of big / small */
+function ceiling(small, big) {
+    return intDivisionRemainder(small, big) === 0
+        ? intDivisionQuotient(small, big)
+        : intDivisionQuotient(small, big) + 1;
+}
+/* finds the quotient of big / small, found when big is no longer bigger than small */
+function intDivisionQuotient(small, big) {
+    return small <= big
+        ? 1 + intDivisionQuotient(small, big - small)
+        : 0;
+}
+/* finds the remainder of big / small, found when big is no longer bigger than small */
+function intDivisionRemainder(small, big) {
+    return small <= big
+        ? intDivisionRemainder(small, big - small)
+        : big;
+}
+/* function that fractions use, can be used across all ef algos potentially for the fractions */
+function make_frac(num, den) {
+    return {numerator: num,
+        denominator: den};
+}
+function getDen(frac) {
+    return frac.denominator;
+}
+function getNum(frac) {
+    return frac.numerator;
+}
+function print(frac) {
+    console.log("The fraction is " + getNum(frac) + " / " + getDen(frac));
+}
+function simplify(frac) { //can see difference for 4/21
+    const greatestCommonDen = gcd(getNum(frac), getDen(frac));
+    return make_frac(getNum(frac) / greatestCommonDen, getDen(frac) / greatestCommonDen);
+}
+function gcd(small, big) { //iterative
+    if (small === 0) {
+        return big;
+    } else {
+        const remainder = big % small;
+        return gcd(remainder, small);
+    }
+}
+
+const frac1 = make_frac(4, 21);
+console.log(greedy(frac1));
+
+console.log(greedy(make_frac(3, 7)));
---
+++ greedyDiffImp/greedy_frac_rec_ll.js
@@ -0,0 +1,100 @@
+/* Greedy method with chapter 2 data abstraction
+ * naming of elements with const
+ * recursive calling functions (greedy, intDivision functions)
+ * using fraction objects
+ * returns a linked list based on pairs */
+function greedy(frac) {
+    if (getNum(frac) === 0) {
+        return null;
+    } else {
+        //largest unit fraction = the integer value that is at least den / num
+        const unit_fraction_den = ceiling(getNum(frac), getDen(frac));
+        const next_fraction = simplify(
+            make_frac(getNum(frac) * unit_fraction_den - getDen(frac), getDen(frac) * unit_fraction_den)
+            );
+        return createList(unit_fraction_den, greedy(next_fraction));
+    }
+}
+/* similar format to functions found in chapter 1
+ * getting the ceiling of big / small */
+function ceiling(small, big) {
+    return intDivisionRemainder(small, big) === 0
+        ? intDivisionQuotient(small, big)
+        : intDivisionQuotient(small, big) + 1;
+}
+/* finds the quotient of big / small, found when big is no longer bigger than small */
+function intDivisionQuotient(small, big) {
+    return small <= big
+        ? 1 + intDivisionQuotient(small, big - small)
+        : 0;
+}
+/* finds the remainder of big / small, found when big is no longer bigger than small */
+function intDivisionRemainder(small, big) {
+    return small <= big
+        ? intDivisionRemainder(small, big - small)
+        : big;
+}
+/* function that fractions use, can be used across all ef algos potentially for the fractions */
+function make_frac(num, den) {
+    return {numerator: num,
+        denominator: den};
+}
+function getDen(frac) {
+    return frac.denominator;
+}
+function getNum(frac) {
+    return frac.numerator;
+}
+function print(frac) {
+    console.log("The fraction is " + getNum(frac) + " / " + getDen(frac));
+}
+function simplify(frac) { //can see difference for 4/21
+    const greatestCommonDen = gcd(getNum(frac), getDen(frac));
+    return make_frac(getNum(frac) / greatestCommonDen, getDen(frac) / greatestCommonDen);
+}
+function gcd(small, big) { //iterative
+    if (small === 0) {
+        return big;
+    } else {
+        const remainder = big % small;
+        return gcd(remainder, small);
+    }
+}
+
+/* pair is a primitive function in the environment given by the book
+ * implemented like how the book says */
+function pair (num, den) {
+    return m => m === 0
+        ? num
+        : m === 1
+            ? den
+            : Error("Argument not 0 or 1 in pair", m);
+}
+function head(p) { //first element of pair
+    return p(0);
+}
+function tail(p) { //second element of pair
+    return p(1);
+}
+/* constructor for list */
+function createList(first, second) {
+    return pair(first, second);
+}
+/* function to add a new thing (toBeAdded) to linked list */
+function addToList(p, toBeAdded) {
+    return tail(p) === null
+        ? pair(head(p), pair(toBeAdded, null))
+        : pair(head(p), addToList(tail(p), toBeAdded));
+}
+/* to be able to see linked list */
+function stringifyList(p) {
+    if (p === null) {
+        return null;
+    }
+    return "[" + head(p) + ", " + stringifyList(tail(p)) + "]";
+}
+
+const frac1 = make_frac(4, 21);
+console.log(stringifyList(greedy(frac1)));
+
+console.log(stringifyList(greedy(make_frac(3, 7))));
